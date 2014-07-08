b6b8d2e8c3499877a457f92a88dd615fcf9b05a3
Yurchik Oleg <oleg.yurchik@gmail.com>
2014-07-08 16:22:28 +0400

    Изменены тесты и проверяющий модуль для MUL_ND_N, удалён лишний исполняемый файл

--- tests modules/MUL_ND_N.cpp
+++ tests modules/MUL_ND_N.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <iostream>
 #include "../struct.h"
 natural MUL_ND_N(const natural, const unsigned short); //user function
 natural ADD_NN_N(const natural, const natural); //для user function
@@ -9,28 +10,33 @@ natural NEWN_N_N(natural); //для DELZ_N_N
 void DELN_N(natural); //для DELZ_N_N
 natural MUL_Nk_N(const natural, const unsigned short); //для user function
 bool test(const natural, const natural); //для проверки
-#include "../Модули/MUL_ND_N.cpp" //user function
-#include "../Модули/ADD_NN_N.cpp" //для user function
-#include "../Модули/NZER_N_B.cpp" //для COM_NN_D
-#include "../Модули/DELZ_N_N.cpp" //для NZER_N_B
-#include "../Модули/NEWN_N_N.cpp" //для DELZ_N_N
-#include "../Модули/DELN_N.cpp" //для DELZ_N_N
-#include "../Модули/MUL_Nk_N.cpp" //для user function
-#include "../Модули/COM_NN_D.cpp" //для ADD_NN_N
+#include "../modules/MUL_ND_N.cpp" //user function
+#include "../modules/ADD_NN_N.cpp" //для user function
+#include "../modules/NZER_N_B.cpp" //для COM_NN_D
+#include "../modules/DELZ_N_N.cpp" //для NZER_N_B
+#include "../modules/NEWN_N_N.cpp" //для DELZ_N_N
+#include "../modules/DELN_N.cpp" //для DELZ_N_N
+#include "../modules/MUL_Nk_N.cpp" //для user function
+#include "../modules/COM_NN_D.cpp" //для ADD_NN_N
 #include "test.cpp" //для проверки
 using namespace std;
 
 int main()
 {
-	fstream file("../Тесты/MUL_ND_N",ios::in);
-	unsigned short n;
-	if (!file) return 254; //Проверка на существование тестов
-	file>>n;
-	for(register unsigned short i=1;i<=n;i++)
+	fstream file("../tests/MUL_ND_N",ios::in);
+	unsigned short n=1;
+	if (!file) //Проверка на существование тестов
+	{
+		cout<<"Проверяющие тесты не найдены!\n";
+		return 255;
+	};
+	while (!file.eof())
 	{
 		natural x,_x,z,result; //Копируем x в _x
 		unsigned short d;
+		char comment[256];
 		file>>x.count;
+		if (file.eof()) return 0;
 		_x.count=x.count;
 		x=NEWN_N_N(x);
 		_x=NEWN_N_N(_x);
@@ -43,13 +49,27 @@ int main()
 		result=NEWN_N_N(result);
 		for(register unsigned short j=0;j<result.count;j++)
 			file>>result.digits[j];
+		file.ignore();
+		file.getline(comment,255);
+		file.sync(); //Чтение комментария к тесту
 		z=MUL_ND_N(x,d);//user function
-		if (!test(x,_x)) return 255; //Проверка на сохранность исходных данных
-		if (!test(z/*user function*/,result)) return i;
+		if (!test(x,_x)) //Проверка на сохранность исходных данных
+		{
+			cout<<"Ошибка! Данные, переданные в модуль, были изменены!";
+			return 254;
+		};
+		if (!test(z/*user function*/,result))
+		{
+			cout<<"Ошибка в тесте "<<n<<":\n";
+			for(register unsigned char i=0;comment[i];i++)
+				cout<<comment[i];
+			return n;
+		};
 		DELN_N(x);
 		DELN_N(_x);
 		DELN_N(z);
 		DELN_N(result);
+		n++;
 	};
 	return 0;
 };
