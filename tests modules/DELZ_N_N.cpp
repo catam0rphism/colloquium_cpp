90d73acf44d6271a6440f39494d737bd1995af8e
Юрчик Олег <oleg.yurchik@gmail.com>
2014-10-09 01:13:48 +0400

    Исправлен DELZ_N_N, исправлены тесты к DELZ_N_N, исправления не тестировались на компиляторе, перепроверить и снова закомитить в случае нахождения ошибок

--- tests modules/DELZ_N_N.cpp
+++ tests modules/DELZ_N_N.cpp
@@ -6,22 +6,27 @@ natural NEWN_N_N(natural n); //для user function
 natural DELZ_N_N(const natural n); //user function
 bool test(const natural, const natural); //для проверки
 void DELN_N(natural); //для user function
-#include "../Модули/DELZ_N_N.cpp" //user function
-#include "../Модули/DELN_N.cpp" //для user function
+#include "../modules/DELZ_N_N.cpp" //user function
+#include "../modules/DELN_N.cpp" //для user function
 #include "test.cpp" //для проверки
-#include "../Модули/NEWN_N_N.cpp" //для user function
+#include "../modules/NEWN_N_N.cpp" //для user function
 //using namespace std;
 
 int main()
 {
-	unsigned short n=0;
-	fstream file("../Тесты/DELZ_N_N",ios::in);
-	if (!file) return 254; //Проверка на существование тестов
-	file>>n;
-	for(register unsigned short i=1; i<=n; i++)
+	unsigned short n=1;
+	char comment[256];
+	fstream file("../tests/DELZ_N_N",ios::in);
+	if (!file) //Проверка на существование тестов
+	{
+		cout<<"Проверяющие тесты не найдены!\n";
+		return 255;
+	};
+	while (!file.eof())
 	{
 		natural x,_x,result,z; //Копируем x в _x
 		file>>x.count;
+		if (file.eof()) return 0;
 		_x.count=x.count;
 		x=NEWN_N_N(x);
 		_x=NEWN_N_N(_x);
@@ -34,13 +39,27 @@ int main()
 		result=NEWN_N_N(result);
 		for(register unsigned short j=0;j<result.count;j++)
 			file>>result.digits[j];
+		file.ignore();
+		file.getline(comment,255);
+		file.sync();
 		z=DELZ_N_N(x);//user function
-		if (!test(x,_x)) return 255; //Проверка на сохранность данных
-		if (!test(z/*user function*/,result)) return i;
+		if (!test(x,_x)) //Проверка
+		{
+			cout<<"Ошибка! Данные, переданные в модуль, были изменены!";
+			return 254;
+		};
+		if (!test(z/*user function*/,result))
+		{
+			cout<<"Ошибка в тесте "<<n<<":\n";
+			for(register unsigned char i=0;comment[i];i++)
+				cout<<comment[i];
+			return n;
+		};
 		DELN_N(x);
 		DELN_N(_x);
 		DELN_N(z);
 		DELN_N(result);
+		n++;
 	};
 	return 0;
 }
