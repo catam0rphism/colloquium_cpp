133871ea9227985751d0d46da5091e18a24b5c40
Dmitriy Belkin <demondima123456@gmail.com>
2015-04-18 23:36:22 +0300

    Небольшие стилистические изменения

--- main.cpp
+++ main.cpp
@@ -2,19 +2,18 @@
 #include <iostream>
 #include "modules/natural/natural.h"
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
     // int counter = 0;
 
     natural a({ 6, 4, 3, 2, 1});
-    
+
     // std::cout << counter++ << std::endl;
     natural b = a;
 
     std::cout << b.digits.size() << std::endl;
 
     // std::cout << counter++ << std::endl;
-    auto c = ADD_NN_N(a,b);
+    auto c = ADD_NN_N(a, b);
 
     // std::cout << counter++ << std::endl;
     for (int i = 0; i <= c.order(); i++) {
