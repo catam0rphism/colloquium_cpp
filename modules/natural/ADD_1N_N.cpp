102c4f234f398a7fe489f0e65530fd7d983d9992
Dmitriy Belkin <demondima123456@gmail.com>
2015-04-18 19:59:20 +0300

    Ещё немного рефакторинга

--- modules/natural/ADD_1N_N.cpp
+++ modules/natural/ADD_1N_N.cpp
@@ -1,10 +1,17 @@
-natural ADD_1N_N(const natural n) {
-	natural res = n;
-	res.digits[0]++;
+// Copyright 2015 Belkin Dmitriy
+#ifndef ADD_1N_N_CPP
+#define ADD_1N_N_CPP
 
-	if(res.digits[0] == 10) {
-		res.digits[0]-= 10;
-		res.digits[1]++;
-	}
-	return res;
-};
+natural ADD_1N_N(const natural n) {
+    natural res = n;
+    res.digits[0]++;
+
+    // Сдвигаем разряд при переполнении
+    if (res.digits[0] == 10) {
+        res.digits[0]-= 10;
+        res.digits[1]++;
+    }
+    return res;
+}
+
+#endif  // ADD_1N_N_CPP
