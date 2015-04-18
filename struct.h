df76327bf7c4986e419443f735253dc6c20f8ec2
Dmitriy Belkin <demondima123456@gmail.com>
2015-04-18 19:51:19 +0300

    Немного рефакторинга

--- struct.h
+++ struct.h
@@ -1,17 +1,17 @@
+// Copyright 2015 Belkin Dmitriy
 #ifndef STRUCT_H
 #define STRUCT_H
 
 #include <vector>
 
 //Натуральное число
-struct natural
-{
+struct natural {
     natural(const natural& number) {
-        this->digits = digits; // vector copy constructor
+        this->digits = digits;  // vector copy constructor
     }
 
     //Массив чисел
-    std::vector<short int> digits;
+    std::vector<int16> digits;
 
     //Количество разрядов в числе
     // TODO: test it
@@ -21,25 +21,27 @@ struct natural
         digits->clean();
         delete digits;
     }
-    
+
     // TODO: переопределить операторы
 };
-struct integer //Целое число
-{
-	bool sign; //Знак числа
-	natural module; //Модуль целого числа, представленный как натуральное
-};
-struct fraction //Дробь
-{
-	integer numerator; //Числитель дроби, представленный как целое число
-	natural denominator; //Знаменатель дроби, представленный как натуральное число
-};
-struct polynom //Многочлен
-{    
-    std::vector<fraction>* coefficients; //Коэффициенты многочлена, представленные дробью
 
-    //Степень многочлена
-    int degree() { return coefficients->length(); }
-};
+// Посмотреть на это все позже =)
+// struct integer //Целое число
+// {
+// 	bool sign; //Знак числа
+// 	natural module; //Модуль целого числа, представленный как натуральное
+// };
+// struct fraction //Дробь
+// {
+// 	integer numerator; //Числитель дроби, представленный как целое число
+// 	natural denominator; //Знаменатель дроби, представленный как натуральное число
+// };
+// struct polynom //Многочлен
+// {    
+//     std::vector<fraction>* coefficients; //Коэффициенты многочлена, представленные дробью
+
+//     //Степень многочлена
+//     int degree() { return coefficients->length(); }
+// };
 
 #endif // STRUCT_H
