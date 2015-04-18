916518fbaa26a419393c66f2311f8d4f90addd7a
Dmitriy Belkin <demondima123456@gmail.com>
2015-04-18 19:38:49 +0300

    Переписал функцию инкремента

--- modules/natural/ADD_1N_N.cpp
+++ modules/natural/ADD_1N_N.cpp
@@ -1,24 +1,10 @@
-natural ADD_1N_N(const natural n)
-{
-	natural x,y;
-	x=DELZ_N_N(n);
-	y.count=x.count;
-	unsigned short i=0;
-	for(;i<x.count && x.digits[i]==9;i++);
-	if (i==x.count) 
-	{
-		y.count++;
-		y=NEW_N_N(y);
-		for(register unsigned short j=0;j<y.count-1;j++)
-			y.digits[j]=0;
-		y.digits[y.count-1]=1;
+natural ADD_1N_N(const natural n) {
+	natural res = n;
+	res.digits[0]++;
+
+	if(res.digits[0] == 10) {
+		res.digits[0]-= 10;
+		res.digits[1]++;
 	}
-	else
-	{
-		y=COPY_N_N(x);
-		for(register unsigned short j=0;j<i;j++)
-			y.digits[j]=0;
-		y.digits[i]++;
-	};
-	return y;
+	return res;
 };
