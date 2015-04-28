bf2d2c60d4762648a991235ac4b82b1903b318f8
NeeChoo <chusov.n@ya.ru>
2015-04-28 19:44:01 +0300

    Create ADD_QQ_Q.cpp

---
+++ modules/fractional/ADD_QQ_Q.cpp
@@ -0,0 +1,17 @@
+//4373 28.04.15 Omeluk, Mokeev
+
+fraction ADD_QQ_Q(fraction a, fraction b)
+{
+	fraction sum = 0;
+	if (b.denominator == a.denominator)
+	{	
+		sum.denominator = b.denominator;
+		sum.numerator = ADD_ZZ_Z(a.numerator,b.numerator);
+		return sum;
+	}
+	else{
+		sum.denominator = LCM_NN_N(a, b);
+		sum.numerator = ADD_ZZ_Z((MUL_ZZ_Z(a.numerator, TRANS_N_Z(sum.denominator / a.denominator))), MUL_ZZ_Z(b.numerator, TRANS_N_Z(sum.denominator / b.denominator)));
+		return sum;
+	}
+}
