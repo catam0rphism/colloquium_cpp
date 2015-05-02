5a016e6132557d0ca8c1b6ef3914634f3b6cb2df
Dmitriy Belkin <demondima123456@gmail.com>
2015-05-03 00:02:24 +0300

    Исправил ошибку при вызове, к примеру, с 99 и 11

--- modules/natural/DIV_NN_Dk.cpp
+++ modules/natural/DIV_NN_Dk.cpp
@@ -16,17 +16,21 @@ natural DIV_NN_Dk(const natural& a, const natural& b) {
     if (NZER_N_B(b)) throw std::invalid_argument("Second operand is equal to zero");
 
     natural res = a;
-    unsigned n;
-    n = a.order() - b.order();
-    natural sub = MUL_Nk_N(b, n);
-    natural k = _0;
+    std::int8_t n = a.order() - b.order();
 
-    while (COM_NN_D(res, sub) != ordinal::GT) {
-        n--;
+    natural sub = MUL_Nk_N(b, n);
+    natural k;  // ?
+
+    // while (COM_NN_D(res, sub) != ordinal::GT) {
+    //     n--;
+    //     sub = MUL_Nk_N(b, n);
+    // }
+    if(COM_NN_D(res, sub) != ordinal::GT) {
+        n -= 1;
         sub = MUL_Nk_N(b, n);
     }
 
-    while (COM_NN_D(res, sub) == ordinal::GT) {
+    while (COM_NN_D(res, sub) != ordinal::LT) {
         res = SUB_NN_N(res, sub);
         k = ADD_1N_N(k);
     }
