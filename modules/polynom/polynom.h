8a8debd65c3fa9d602c28f79a5a4fbfa727d06b6
Tim Catler <timecatler@gmail.com>
2015-05-05 09:37:46 +0300

    Больше модулей для полиномов

--- modules/polynom/polynom.h
+++ modules/polynom/polynom.h
@@ -2,10 +2,13 @@
 #ifndef POLYNOM_H
 #define POLYNOM_H
 
+#include "ADD_PP_P.cpp"
 #include "DEG_P_N.cpp"
-#include "SUB_PP_P.cpp"
+#include "DIV_PP_P.cpp"
+#include "LED_P_Q.cpp"
+#include "MOD_PP_P.cpp"
 #include "MUL_PQ_P.cpp"
 #include "MUL_Pxk_P.cpp"
-#include "LED_P_Q.cpp"
+#include "SUB_PP_P.cpp"
 
 #endif  // POLYNOM_H
