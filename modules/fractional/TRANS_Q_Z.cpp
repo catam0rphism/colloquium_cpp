b9591dfa8ab8e0abc35ffa22eb6e33cd0b560a94
Tim Catler <timecatler@gmail.com>
2015-05-15 19:33:19 +0300

    Добавил документацию для рациональных
    Мелкие фиксы.

---
+++ modules/fractional/TRANS_Q_Z.cpp
@@ -0,0 +1,13 @@
+// Copyright Солдаткина и Сливенская, группа 4373
+// 26.04.2015
+#ifndef TRANS_Q_Z_CPP
+#define TRANS_Q_Z_CPP
+
+#include "../../structs/struct.h"
+
+integer TRANS_Q_Z(fraction Q) {
+    //возвращает числитель дроби
+    return Q.numerator;
+}
+
+#endif  // TRANS_Q_Z_CPP
